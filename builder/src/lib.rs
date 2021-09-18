3cb02623cd5fb37d9322887ce10ccc42db703c70
curtain <kaleidoscope@163.com>
2021-09-18 17:37:09 +0800

    impl first test case

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream;
+use syn;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let _st = syn::parse_macro_input!(input as syn::DeriveInput);
+    TokenStream::new()
 }
